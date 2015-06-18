ffee1b75bd4e6999636313ce497c9fb08c8aac89
Joey Poon <joey@joeypoon.com>
2015-06-18 13:37:33 -0500

    user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      redirect_to root_path, notice: '😎'
+    else
+      flash.now[:alert] = '💩'
+      render :new
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :password_digest)
+    end
+
+end
